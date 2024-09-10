32ef3f5e6eda3a4428b18215c43e51c2084b65c9
Luciano T. <tonetlds@gmail.com>
2024-09-10 19:50:55 -0300

    Adiciona changelog e script para geração automática
    Inclui um novo arquivo de changelog para documentar as alterações no projeto de forma clara e acessível. Com a adição do script de geração automática, é possível atualizar facilmente o changelog com base nos commits do GitHub, garantindo que as informações relevantes estejam sempre disponíveis. Essas melhorias visam facilitar a comunicação de mudanças entre a equipe e usuários, além de manter a documentação mais organizada e atualizada.

---
+++ CHANGELOG_GEN.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Configurações
+CHANGELOG_FILE="CHANGELOG.md"
+TEMP_FILE="temp_changelog.md"
+REPO_OWNER="lucianotonet"
+REPO_NAME="sistep_admin"
+GITHUB_API="https://api.github.com"
+
+# Verifica se o curl está instalado
+if ! command -v curl &> /dev/null; then
+    echo "Error: curl não está instalado."
+    exit 1
+fi
+
+# Verifica se há permissão de escrita
+if [ ! -w "$CHANGELOG_FILE" ]; then
+    echo "Error: Permissão negada para escrever no $CHANGELOG_FILE."
+    exit 1
+fi
+
+# Inicializa o arquivo temporário do Changelog
+echo "# Changelog" > "$TEMP_FILE"
+
+# Obtém os commits do GitHub
+commits=$(curl -s "$GITHUB_API/repos/$REPO_OWNER/$REPO_NAME/commits")
+
+# Gera o changelog baseado nos commits do GitHub
+echo "$commits" | jq -r '.[] | "_[" + .commit.author.date + "](https://github.com/'$REPO_OWNER'/'$REPO_NAME'/commits/" + .sha + ")_\n### " + .commit.message + "\n\n"' >> "$TEMP_FILE"
+
+# Atualiza o arquivo Changelog com o temporário
+if mv "$TEMP_FILE" "$CHANGELOG_FILE"; then
+    if ! cp "$CHANGELOG_FILE" "./public/$CHANGELOG_FILE"; then
+        echo "Error: Falha ao copiar $CHANGELOG_FILE para /public/."
+        exit 1
+    fi
+else
+    echo "Error: Falha ao mover $TEMP_FILE para $CHANGELOG_FILE."
+    exit 1
+fi
+
+echo "Changelog atualizado com sucesso!"
