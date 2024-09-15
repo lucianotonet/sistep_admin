cb65ec67b2f9d92efeb241ecc24ed3a60499c62d
Luciano T. <tonetlds@gmail.com>
2024-09-15 05:20:53 -0300

    feat: redesign welcome view and enhance user experience
    - Refatoração da visualização de boas-vindas com uma nova estrutura que inclui um título e uma descrição clara da plataforma, melhorando a apresentação inicial para novos usuários.
    - Implementação de um esquema de cores compatível com o modo escuro, garantindo acessibilidade e uma experiência de usuário consistente em diferentes configurações de visualização.
    - Melhoria na navegação de autenticação, simplificando as opções disponíveis para autenticação e registro, facilitando o acesso para os usuários.
    
    Essas alterações visam aumentar o envolvimento do usuário e a usabilidade da interface, oferecendo uma apresentação mais atrativa e acessível da plataforma.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,31 @@
 <x-guest-layout>
-    <div class="container px-4 py-12">
-        <nav class="flex flex-col gap-10 items-center justify-center sm:space-x-4 sm:space-y-0">
-            @if (Route::has("login"))
-                @auth
-                    <div class="flex flex-col items-center gap-2 justify-center">
-
-                        <img src="https://www.gravatar.com/avatar/{{ md5(strtolower(trim(auth()->user()->email))) }}?d=identicon" alt="Foto de perfil" class="w-12 h-12 rounded-full">
-                        <p>
-                            Olá, {{ auth()->user()->name }}
-                        </p>
-                    </div>
-                    <a href="{{ url("/dashboard") }}"
-                        class="button bg-primary text-primary-foreground hover:bg-primary/90 transition-colors">
-                        Acessar Painel
-                    </a>
-                @else
-                    <div class="flex flex-row items-center gap-4 justify-center">
-
-                        <a href="{{ route("login") }}" class="button bg-primary text-primary-foreground hover:bg-primary/90 transition-colors">Entrar</a>
+    <div class="container px-4 py-12 dark:text-white">
+        <div class="flex flex-col items-center justify-center">
+            <h1 class="text-4xl font-bold mb-4">SISTEP</h1>
+            <p class="text-lg mb-8">
+                Plataforma de aplicação e correção de instrumentos psicológicos não privativos
+            </p>
+            <nav class="flex flex-col gap-4 items-center justify-center sm:flex-row sm:space-x-4 sm:space-y-0">
+                @if (Route::has("login"))
+                    @auth
+                        <div class="flex flex-col items-center gap-2 justify-center">
+                            <img src="https://www.gravatar.com/avatar/{{ md5(strtolower(trim(auth()->user()->email))) }}?d=identicon" alt="Foto de perfil" class="w-12 h-12 rounded-full">
+                            <p>Olá, {{ auth()->user()->name }}</p>
+                        </div>
+                        <a href="{{ url("/dashboard") }}"
+                            class="button bg-primary text-primary-foreground hover:bg-primary/90 transition-colors dark:bg-darkmode-primary dark:text-darkmode-primary-foreground dark:hover:bg-darkmode-primary/90">
+                            Acessar Painel
+                        </a>
+                    @else
+                        <a href="{{ route("login") }}" class="button bg-primary text-primary-foreground hover:bg-primary/90 transition-colors dark:bg-darkmode-primary dark:text-darkmode-primary-foreground dark:hover:bg-darkmode-primary/90">Entrar</a>
                         @if (Route::has("register"))
-                            <a href="{{ route("register") }}" class="button bg-primary text-primary-foreground hover:bg-primary/90 transition-colors">
+                            <a href="{{ route("register") }}" class="button bg-primary text-primary-foreground hover:bg-primary/90 transition-colors dark:bg-darkmode-primary dark:text-darkmode-primary-foreground dark:hover:bg-darkmode-primary/90">
                                 Criar conta
                             </a>
                         @endif
-                    </div>
-                @endauth
-            @endif
-        </nav>
+                    @endauth
+                @endif
+            </nav>
+        </div>
     </div>
 </x-guest-layout>
